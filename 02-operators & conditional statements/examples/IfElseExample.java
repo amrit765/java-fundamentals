829b81cd9f406016b73aa1780df4f5b4dce36674
Amrit Raj <amrit7ml@gmail.com>
2026-04-09 22:39:58 +0530

    Add files via upload

---
+++ 02-operators & conditional statements/examples/IfElseExample.java
@@ -0,0 +1,13 @@
+//Demostration of If else.
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int age = 22;
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+    }
+}
