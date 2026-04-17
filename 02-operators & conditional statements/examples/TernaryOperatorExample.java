1d162119ad2ea18caebfd09c89aa81fe6d6fa939
Amrit Raj <amrit7ml@gmail.com>
2026-04-17 21:41:50 +0530

    Add files via upload

---
+++ 02-operators & conditional statements/examples/TernaryOperatorExample.java
@@ -0,0 +1,15 @@
+// Demonstrates ternary operator
+
+public class TernaryOperatorExample {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        // Ternary operator
+        int larger = (a > b) ? a : b;
+
+        System.out.println("Larger number = " + larger);
+    }
+}
