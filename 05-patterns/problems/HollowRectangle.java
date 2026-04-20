86d075018df7fc6fc43f74d76d0eae59cead26ca
Amrit Raj <amrit7ml@gmail.com>
2026-04-20 22:06:14 +0530

    Add files via upload

---
+++ 05-patterns/problems/HollowRectangle.java
@@ -0,0 +1,39 @@
+// Q. Print Hollow Rectangle Pattern
+    // *****
+    // *   *
+    // *   *
+    // *****
+
+    // Logic:
+    // Boundary cells → print '*'
+    // Inner cells → print space
+    // Boundary condition:
+    // i == 1 OR i == totRows OR j == 1 OR j == totCols
+
+    public class HollowRectangle {
+
+    public static void hollow_rectangle(int totRows, int totCols) {
+
+        // outer loop
+        for(int i=1; i<=totRows; i++) {
+
+            // inner - columns
+            for(int j=1; j<=totCols; j++) {
+
+                // cell (i, j)
+                if(i == 1 || i == totRows || j == 1 || j == totCols) {
+                    // boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        hollow_rectangle(4, 5);
+    }
+}
