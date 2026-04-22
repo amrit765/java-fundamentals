5070649d85a696703fdf40d31d48b9e8443e3131
Amrit Raj <amrit7ml@gmail.com>
2026-04-22 20:48:51 +0530

    Add files via upload

---
+++ 05-patterns/problems/Diamond.java
@@ -0,0 +1,62 @@
+// Q. Print Diamond Pattern
+    /*
+        *
+       ***
+      *****
+     *******
+     *******
+      *****
+       ***
+        *
+    */
+
+    // Logic:
+    // Pattern has 2 halves:
+    // 1st half → increasing stars (odd numbers)
+    // 2nd half → decreasing stars
+    // Spaces → (n - i)
+    // Stars → (2*i - 1)
+    // 
+    
+public class Diamond {
+
+
+    public static void diamond(int n) {
+
+        // 1st half
+        for(int i=1; i<=n; i++) {
+
+            // spaces
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // 2nd half
+        for(int i=n; i>=1; i--) {
+
+            // spaces
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for(int j=1; j<=(2*i)-1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        diamond(4);
+    }
+}
