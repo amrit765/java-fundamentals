0a0598455ac83a1b8367bf86f59d00c167e73462
Amrit Raj <amrit7ml@gmail.com>
2026-05-01 20:25:40 +0530

    Add files via upload

---
+++ 07-strings/examples/CharAtExample.java
@@ -0,0 +1,13 @@
+//Demonstrates how to access characters in a string
+
+public class CharAtExample {
+    public static void main(String[] args) {
+
+        String fullName = "Aishwarya";
+
+        // Access first character
+        char firstChar = fullName.charAt(0);
+
+        System.out.println("First character: " + firstChar);
+    }
+}
---
+++ 07-strings/examples/StringConcatenation.java
@@ -0,0 +1,14 @@
+// Demonstrates string concatenation
+ 
+public class StringConcatenation {
+    public static void main(String[] args) {
+
+        String firstName = "Aishwarya";
+        String lastName = "Shree";
+
+        // Concatenation using +
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
